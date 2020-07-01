e7017d78dcd04b14f9693262bae171fd09c627eb
LachelleF <lachellefryett@gmail.com>
2020-06-30 20:32:22 -0400

    Server unit test. WIP

--- server/index.js
+++ server/index.js
@@ -22,25 +22,29 @@ app.get('PriceAndPromotion/:product_id', (req, res) => {
   let id = req.params.product_id;
   console.log('Patch req recvd urlId: ', id);
 
-  db.findOne({ _id: id })
-    .then((game) => {
-      if (!game) {
-        res.status(400).send('No game to return');
-      } else {
-        res.status(200).send({ game });
+  getGameDocument(id);
 
-      }
-    })
-    .catch((err) => {
-      res.status(404).send(err);
-    });
 });
+
+let getGameDocument = (id) => {
+  db.findOne({ _id: id })
+  .then((game) => {
+    if (!game) {
+      res.status(400).send('No game to return');
+    } else {
+      res.status(200).send({ game });
+    }
+  })
+  .catch((err) => {
+    res.status(404).send(err);
+  });
+}
 
 app.listen(PORT, (error) => {
   if (error) {
     console.log('Server connection failed: ', error);
   }
   console.log('Server listening on port ', PORT);
-});*/
+});
 
-
+module.exports = getGameDocument
