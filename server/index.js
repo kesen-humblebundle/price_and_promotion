9f4f200b93e2b6825961fdb66a698b48592549b5
LachelleF <lachellefryett@gmail.com>
2020-07-18 16:00:35 -0400

    broke endpoints into two to reduce risk

--- server/index.js
+++ server/index.js
@@ -13,11 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-// app.get('/', (req, res) => {
-//   res.sendFile('index.html');
-// });
-
-//returns an object that includes price, promotion/discount, start date, and expiry
+//returns an object that includes price, promotion/discount
 app.get('/PriceAndPromotion/:product_id', (req, res) => {
   console.log('Req rcvd by server with:', req.params);
   let id = req.params.product_id;
@@ -41,60 +37,32 @@ app.get('/PriceAndPromotion/:product_id', (req, res) => {
     });
 });
 
-//returns an object that includes price, promotion/discount, start date, and expiry
-app.get('/PriceAndPromotion/:product_id', (req, res) => {
+//returns price and promos for an array of product ids
+app.get('/PriceAndPromotion/multiple/:product_id', (req, res) => {
   console.log('Req rcvd by server with:', req.params);
-  let id = req.params.product_id;
-  let type = req.query.type;
-  console.log('urlId: ', id);
-  console.log('query param type: ', type);
-
-  //supports the return of price and promos for an array of product ids
-  if (type === 'carousel' && id) {
-    let arrayOfIds = JSON.parse(req.params.productId);
-    let dataBundle = async () => {
-      let priceAndPromosArray = []
-      for (let i = 0; i < arrayOfIds.length; i++) {
-        await Images.find({ product_id: arrayOfIds[i] })
-          .then((docs) => {
-            let data = {
-              product_id: docs[0].product_id,
-              price: docs[0].price,
-              promotion: docs[0].discount
-            }
-            priceAndPromosArray.push(data);
-            console.log('In server, price and promo array from db: ', priceAndPromosArray);
-          })
-          .catch(err => console.log(err))
-      }
-      return priceAndPromosArray;
-    }
-    Promise.resolve(dataBundle()).then((data) => {
-      console.log('shape of PriceAndPromo data: ', data)
-      console.log('*sending data back to Other Popular Games client')
-      res.status(200).send(data);
-    })
-  } else {
-
-    //supports the return of a single product id's price and promotion data
-    PriceAndPromo.find({ product_id: id })
-      .then(game => {
-        console.log('data from db: ', game);
-        if (!game) {
-          res.status(400).send('No game to return');
-        } else {
+  let arrayOfIds = JSON.parse(req.params.productId);
+  let dataBundle = async () => {
+    let priceAndPromosArray = []
+    for (let i = 0; i < arrayOfIds.length; i++) {
+      await PriceAndPromo.find({ product_id: arrayOfIds[i] })
+        .then((docs) => {
           let data = {
-            price: game[0].price,
-            promotion: game[0].discount,
+            product_id: docs[0].product_id,
+            price: docs[0].price,
+            promotion: docs[0].discount
           }
-          console.log('data ready to go to client: ', data);
-          res.status(200).send(data);
-        }
-      })
-      .catch(err => {
-        res.status(404).send(err);
-      });
+          priceAndPromosArray.push(data);
+          console.log('In server, price and promo array from db: ', priceAndPromosArray);
+        })
+        .catch(err => console.log(err))
+    }
+    return priceAndPromosArray;
   }
+  Promise.resolve(dataBundle()).then((data) => {
+    console.log('Shape of PriceAndPromo data: ', data)
+    console.log('Sending data back to Other Popular Games client')
+    res.status(200).send(data);
+  })
 });
 
 //based on product number from URL,returns an object that includes price, promotion/discount, start date, and expiry
@@ -109,4 +77,6 @@ app.listen(PORT, (error) => {
   console.log('Server listening on port ', PORT);
 });
 
-
+// app.get('/', (req, res) => {
+//   res.sendFile('index.html');
+// });
