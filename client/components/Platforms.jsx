15b94264977d7e587b01983ecc2b76597746b9a8
LachelleF <lachellefryett@gmail.com>
2020-07-21 15:31:57 -0400

    rendering platform gifs

--- client/components/Platforms.jsx
+++ client/components/Platforms.jsx
@@ -10,6 +10,7 @@ import styled from 'styled-components';
 
 const PlatformsStyled = styled.div`
   height: 24px;
+  float: left;
 `;
 
 const Platforms = (props) => {
@@ -19,7 +20,7 @@ const Platforms = (props) => {
   console.log('array of platforms and os: ', combined);
   const platformsArray = combined.map((platform, index) => {
     console.log('platform and index via map: ', platform, index)
-    return <img src={{platform}} alt="platform icon" width="21px" key={index}></img>;
+    return <img src={platform} alt="platform icon" width="21px" key={index}></img>;
   });
   return (
     <PlatformsStyled>
